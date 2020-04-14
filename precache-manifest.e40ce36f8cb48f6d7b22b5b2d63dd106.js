84e3e43c6cb7d0982dd90adef6eb53e44c5eeae5
Dmitiy <github@dimail.ru>
2020-04-14 12:51:04 +0500

    add create-react-app

---
+++ precache-manifest.e40ce36f8cb48f6d7b22b5b2d63dd106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecad5890d8609e9e9ff74c3ea2a4ce9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69b6477e9d85dce6d923",
+    "url": "/static/css/main.2ac2206e.chunk.css"
+  },
+  {
+    "revision": "a254a209b7fab5e7fec6",
+    "url": "/static/js/2.36f66c75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.36f66c75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b6477e9d85dce6d923",
+    "url": "/static/js/main.f871d0a1.chunk.js"
+  },
+  {
+    "revision": "79ba4dc2406c0b4e3cec",
+    "url": "/static/js/runtime-main.de26af3a.js"
+  }
+]);
